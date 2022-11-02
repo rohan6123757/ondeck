bd8fbb2002116fb693de8a1f11fee8d12f575fff
Rohan Roy <97940474+rohan6123757@users.noreply.github.com>
2022-11-03 03:16:49 +0530

    update index.js

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ a.addEventListener("click",function(){
         document.getElementsByClassName("navbar")[0].classList.add("navbar_press");
         document.getElementsByClassName("navItems")[0].classList.add("pressed");
         document.getElementsByClassName("container_two")[0].classList.add("imgPress");
-        // document.getElementsByClassName("navbar")[0].classList.remove("onPress");
         document.getElementById("logo").style.display="none"
     }else{
         a.setAttribute("src","/Assets/Menu.svg");
-        // document.getElementsByClassName("navbar")[0].classList.add("onPress");
         document.getElementsByClassName("navbar")[0].classList.remove("navbar_press");
         document.getElementsByClassName("navItems")[0].classList.remove("pressed");
         document.getElementsByClassName("container_two")[0].classList.remove("imgPress");
