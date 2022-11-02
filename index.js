303d366c74ac53a65fc323559489cc9db189b438
Rohan Roy <97940474+rohan6123757@users.noreply.github.com>
2022-11-03 03:23:31 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let a = document.getElementById("menu");
 a.addEventListener("click",function(){ 
-    if (a.getAttribute("src")!="/Assets/Close.svg"){
-        a.setAttribute("src","/Assets/Close.svg");
+    if (a.getAttribute("src")!="Assets/Close.svg"){
+        a.setAttribute("src","Assets/Close.svg");
         document.getElementsByClassName("navbar")[0].classList.add("navbar_press");
         document.getElementsByClassName("navItems")[0].classList.add("pressed");
         document.getElementsByClassName("container_two")[0].classList.add("imgPress");
         document.getElementById("logo").style.display="none"
     }else{
-        a.setAttribute("src","/Assets/Menu.svg");
+        a.setAttribute("src","Assets/Menu.svg");
         document.getElementsByClassName("navbar")[0].classList.remove("navbar_press");
         document.getElementsByClassName("navItems")[0].classList.remove("pressed");
         document.getElementsByClassName("container_two")[0].classList.remove("imgPress");
